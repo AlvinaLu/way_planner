7f9caf661abad8e023fabad943732852d9aecb49
Lushnikova, Alvina <lushnalv@fel.cvut.cz>
2023-02-27 12:00:57 +0100

    First commit

---
+++ app/src/main/java/cz/cvut/fel/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.baralarska_prace_android_front_end
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.baralarska_prace_android_front_end.ui.theme.Baralarska_prace_android_front_endTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Baralarska_prace_android_front_endTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Baralarska_prace_android_front_endTheme {
+        Greeting("Android")
+    }
+}
